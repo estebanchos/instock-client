938678e8e7dcd5647d0adedebbd8a1dbc86aa2e9
Suraya <surayaclemens@gmail.com>
2022-06-14 22:18:09 -0700

    have to commit so i can pull latest updates from sam and carlos

---
+++ src/components/Inventories/Inventories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Inventories.scss';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
+
+class Inventories extends React.Component {
+
+}
+
+export default Inventories;
