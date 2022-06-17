2d07af927d9e5abaf223461726338cab11256b6a
Carlos Ocampo <charlie@carlosocampo.ca>
2022-06-16 19:33:19 -0500

    form input and validation implemented. Pending API test

--- src/App.js
+++ src/App.js
@@ -6,22 +6,26 @@ import Footer from './components/Footer/Footer';
 import WarehousesPage from './pages/WarehousesPage/WarehousesPage';
 import InventoriesPage from './pages/InventoriesPage/InventoriesPage';
 import NewInventoryPage from './pages/NewInventoryPage/NewInventoryPage';
+import WarehouseForm from './components/WarehouseForm/WarehouseForm';
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-        <BrowserRouter>
+        {/* <BrowserRouter>
           <Header />
           <Switch>
             <Route path="/" exact component={WarehousesPage} />
             <Route path="/inventories" exact component={InventoriesPage} />
-            {/* <Route path="/warehouses/:warehouseId" component={WarehouseDetails} /> */}
+            <Route path="/warehouses/:warehouseId" component={WarehouseDetails} />
             <Route path='/inventories/inventory/new' component={NewInventoryPage} />
+            <Route path='/warehouses/warehouse/new' component={} />
+            
           </Switch> 
           <Footer />
-        </BrowserRouter>
+        </BrowserRouter> */}
+        <WarehouseForm />
       </div>
     );
   }
