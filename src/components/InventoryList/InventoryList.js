42b1cc5a65965894f6c694d8d71c06e2c4934307
Suraya <surayaclemens@gmail.com>
2022-06-19 13:41:39 -0700

    fixing styles, started with inventory list

--- src/components/InventoryList/InventoryList.js
+++ src/components/InventoryList/InventoryList.js
@@ -6,29 +6,36 @@ function InventoryList ({ inventories, deleteClick }){
     return (
         <section className='inventory-section'>
             <div className='inventory-list__filters'>
+                <div className='inventory-list__flex-left'>
                 <p className='inventory-list__filters-title'>
-                    INVENTORY ITEM
+                    Inventory Item
                     <img className='inventory-list__filters-icon'src={sort} alt='arrows'/>
                 </p>
                 <p className='inventory-list__filters-title'>
-                    CATEGORY
+                    Cateogry
+                    <img className='inventory-list__filters-icon' src={sort} alt='arrows'/>
+                </p>
+                </div>
+                <div className='inventory-list__flex-right'>
+                <p className='inventory-list__filters-title'>
+                    Status
                     <img className='inventory-list__filters-icon' src={sort} alt='arrows'/>
                 </p>
                 <p className='inventory-list__filters-title'>
-                    STATUS
+                    Qty
                     <img className='inventory-list__filters-icon' src={sort} alt='arrows'/>
                 </p>
                 <p className='inventory-list__filters-title'>
-                    QTY
+                    Warehouse
                     <img className='inventory-list__filters-icon' src={sort} alt='arrows'/>
                 </p>
+                </div>
+                <div className='inventory-list__flex-right--actions'>
                 <p className='inventory-list__filters-title'>
-                    WAREHOUSE
-                    <img className='inventory-list__filters-icon' src={sort} alt='arrows'/>
+                    Actions
                 </p>
-                <p className='inventory-list__filters-title'>
-                    ACTIONS
-                </p>
+                </div>
+                
             </div>
 
             <ul className='inventory-list'>
